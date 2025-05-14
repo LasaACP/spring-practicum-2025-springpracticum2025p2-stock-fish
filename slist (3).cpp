44febc3dd2f6df40137398c0639ca793665c6393
Jcowbob <Jcowbob@gmail.com>
2025-05-14 10:49:43 -0500

    Jack Martin board code
    Its the board, it is missing some variables for pieces, such as their positions.

---
+++ slist (3).cpp
@@ -0,0 +1,147 @@
+#include "slist.h"
+#include <iostream>
+#include <string>
+using namespace std; 
+
+ piece::piece(){
+    wb = false;
+     point = 0;
+     moved = 0;
+    type = '1';
+    imag = NULL;
+
+
+}
+
+void piece::setPiece(bool color, char t){
+    wb = color;
+    type = t;
+    if(wb == false){
+        if(type == 'p'){
+            point = 1;
+            imag = "♙";
+        }
+        if(type == 'h'){
+            point = 3;
+            imag = "♘";
+        }
+        if(type == 'b'){
+            point = 3;
+            imag = "♗";
+        }
+        if(type == 'r'){
+            point = 5;
+            imag = "♖";
+        }
+        if(type == 'q'){
+            point = 9;
+            imag = "♕";
+        }
+        if(type == 'k'){
+            point = 100;
+            imag = "♔";
+        }
+    }
+    if(wb == true){
+        if(type == 'p'){
+            point = 1;
+            imag = "☗";
+        }
+        if(type == 'h'){
+            point = 3;
+            imag = "♞";
+        }
+        if(type == 'b'){
+            point = 3;
+            imag = "♝";
+        }
+        if(type == 'r'){
+            point = 5;
+            imag = "♜";
+        }
+        if(type == 'q'){
+            point = 9;
+            imag = "♛";
+        }
+        if(type == 'k'){
+            point = 100;
+            imag = "♚";
+        }
+    }
+}
+
+
+
+ tile::tile(){
+     imag = NULL;
+     wb = false;
+     occupied = false;
+     thePiece = NULL;
+}
+
+void tile::setTile(const char* i, bool color, bool oc){
+    imag = i;
+    wb = color;
+    occupied = oc;
+}
+
+void tile::tilePiece(bool color, char t){
+    thePiece->setPiece(color, t);
+    imag = thePiece->imag;
+    occupied = true;
+}
+
+ board::board(){
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            bor[i][j].thePiece = new piece();
+            if((j+i)%2 != 1){
+                bor[i][j].setTile("⬜",true,false);
+            }
+            else{
+                bor[i][j].setTile("⬛",true,false);
+            }
+        }
+    }
+    bor[0][0].tilePiece(false, 'r');
+    bor[0][1].tilePiece(false, 'h');
+    bor[0][2].tilePiece(false, 'b');
+    bor[0][3].tilePiece(false, 'q');
+    bor[0][4].tilePiece(false, 'k');
+    bor[0][5].tilePiece(false, 'b');
+    bor[0][6].tilePiece(false, 'h');
+    bor[0][7].tilePiece(false, 'r');
+    
+    
+    bor[7][0].tilePiece(true, 'r');
+    bor[7][1].tilePiece(true, 'h');
+    bor[7][2].tilePiece(true, 'b');
+    bor[7][3].tilePiece(true, 'q');
+    bor[7][4].tilePiece(true, 'k');
+    bor[7][5].tilePiece(true, 'b');
+    bor[7][6].tilePiece(true, 'h');
+    bor[7][7].tilePiece(true, 'r');
+    
+    for(int i = 0; i < 8; i++){
+        bor[1][i].tilePiece(false, 'p');
+        bor[6][i].tilePiece(true, 'p');
+    }
+
+
+
+}
+
+tile board::getTile(int x, int y){
+    return bor[y][x];
+}
+
+void board::printBoard(){
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            cout << bor[i][j].imag << " ";
+        }
+        cout << endl;
+    }
+}
+
+
---
+++ slist (4).h
@@ -0,0 +1,34 @@
+class piece{
+    public:
+        bool wb;
+        int point;
+        int moved;
+        char type;
+        const char* imag;
+        piece();
+        void setPiece(bool, char);
+};
+
+
+class tile{
+    public:
+        const char* imag;
+        bool wb;
+        bool occupied;
+        piece* thePiece;
+    
+        tile();
+        void setTile(const char*, bool, bool);
+        void tilePiece(bool, char);
+};
+
+
+class board{
+    public:
+        tile bor[8][8];
+        board();
+        tile getTile(int, int);
+        void printBoard();
+};
+
+
