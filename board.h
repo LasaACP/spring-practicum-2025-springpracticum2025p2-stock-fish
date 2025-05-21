dd86d46c499aa063b54ce0775deb6fd45c9d90ef
Jcowbob <Jcowbob@gmail.com>
2025-05-21 11:56:04 -0500

    Update board.h

--- board.h
+++ board.h
@@ -3,6 +3,11 @@
 
 #include "piece.h"
 
+using namespace std;
+
+class piece;
+class Board;
+
 class tile{
     public:
         const char* imag;
@@ -16,13 +21,39 @@ class tile{
 };
 
 
-class board{
+class Board{
     public:
         tile bor[8][8];
-        board();
+        Board();
         tile getTile(int, int);
         void printBoard();
         void moveTile(int, int, piece*);
+};
+
+class piece{
+    public:
+        Board* board;
+
+        piece(Board * b);
+        bool wb;
+        int x;
+        int y;
+        int point;
+        bool moved;
+        string type;
+        
+        const char* imag;
+        vector<string> posMv;
+        void setPiece(bool, int, int, string);
+        void addMove(int, int);
+        void move(int, int);
+        void setPiece(bool, string);
+        void setMoves(piece *, int, int);
+        void movesBish(piece *, int, int);
+        void movesRook(piece *, int, int);
+        void movesPawn(piece *, int, int);
+        void movesKnit(piece *, int, int);
+        void movesKing(piece *, int, int);
 };
 
 #endif
